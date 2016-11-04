fc96acdcce2719ef4de2e44b145200a644a410ec
luoye <842891024@qq.com>
2016-11-04 13:13:50 +0800

    script update

--- script/build.js
+++ script/build.js
@@ -19,22 +19,116 @@ function filter(array) {
 
 booksList = filter(booksList);
 
-function build() {
-	if (!booksList[0]) {
-		logger.success(`All succeed.`);
-		return;
-	};
-	let spinner = ora(`Building ${booksList[0]}`).start();
-	exec(`gitbook build ${path.join(__dirname, '../src/', booksList[0])} ${path.join(__dirname, '../dist/', booksList[0])}`, (err, stdout, stderr) => {
-		if (err) {
-			logger.fatal(err);
-			return;
+const htmlTemplate = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gitbooks · luoye-fe</title>
+	<style type="text/css">
+		.booklist {
+
 		}
-		spinner.stop();
-		logger.success(`Building ${booksList[0]} succeed.`);
-		booksList.splice(0, 1);
-		build();
-	})
+		.booklist li {
+			font-style: normal;
+			list-style: none;
+			line-height: 24px;
+		}
+		.booklist li a {
+			color: #333;
+			text-decoration: none;
+		}
+		.booklist li a:hover {
+			color: #000;
+			text-decoration: underline;
+		}
+	</style>
+</head>
+<body>
+	<ul class="booklist">
+		{{template}}
+	</ul>
+</body>
+</html>
+`;
+
+let index = 0;
+let bookTitle = {};
+function analyseBook() {
+	let spinner = ora(`Analysing books ...`).start();
+	return new Promise((resolve, reject) => {
+		function loop() {
+			if (index >= booksList.length) {
+				index = 0;
+				spinner.stop();
+				logger.success(`Analyse books succeed.`);
+				resolve();
+				return;
+			}
+			fs.readFile(path.join(__dirname, '../src/', booksList[index], 'book.json'), 'utf-8', (err, data) => {
+				let bookJson = JSON.parse(data);
+				bookTitle[booksList[index]] = bookJson.title;
+				index ++;
+				loop();
+			})
+		}
+		loop();
+	});
 }
 
-build();
+function generateIndexHTML() {
+	let spinner = ora(`Generating index HTMl file ...`).start();
+	return new Promise((resolve, reject) => {
+		let template = '';
+		let cur = '';
+		Object.keys(bookTitle).forEach((item) => {
+			cur += `<li><a href="${item}/">${bookTitle[item]}</a></li>`
+		})
+		template = htmlTemplate.replace(/{{template}}/, cur);
+		fs.writeFile(path.join(__dirname, '../dist/index.html'), template, (err) => {
+			if (err) {
+				logger.fatal(err);
+				return;
+			}
+			spinner.stop();
+			logger.success(`Generate index HTMl file succeed.`);
+			resolve();
+		})
+	});
+}
+
+function build() {
+	return new Promise((resolve, reject) => {
+		function loop() {
+			if (index >= booksList.length) {
+				index = 0;
+				resolve();
+				return;
+			}
+			let spinner = ora(`Building ${booksList[index]} ...`).start();
+			exec(`gitbook build ${path.join(__dirname, '../src/', booksList[index])} ${path.join(__dirname, '../dist/', booksList[index])}`, (err, stdout, stderr) => {
+				if (err) {
+					logger.fatal(err);
+					return;
+				}
+				spinner.stop();
+				logger.success(`Building ${booksList[index]} succeed.`);
+				index ++;
+				loop();
+			})
+		}
+		loop();
+	});
+	
+}
+
+analyseBook()
+	.then(() => {
+		return generateIndexHTML();
+	})
+	.then(() => {
+		return build();
+	})
+	.then(() => {
+		logger.success(`All succeed.`);
+	})
