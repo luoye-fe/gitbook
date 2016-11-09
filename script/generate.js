a82a94d29a9be3786a276b1f9006c68cf6ce1a06
luoye <842891024@qq.com>
2016-11-09 16:33:48 +0800

    update

--- script/generate.js
+++ script/generate.js
@@ -4,8 +4,8 @@ import path from 'path';
 import pinyin from 'pinyin';
 import { js as jsbeautify } from 'js-beautify';
 
-let book = 'huì-shì-hòu-sù';
-let catlog = 'xiān-yī-nù-mǎ-shǎo-nián-shí';
+let book = '绘事后素';
+let catlog = '鲜衣怒马少年时';
 
 var pkg = require(`../src/${book}/${catlog}/catlog.json`);
 
@@ -13,15 +13,15 @@ let index = 0;
 
 function write() {
 	if (index >= pkg.length) {
-		fs.writeFileSync(path.join(__dirname, `../src/${book}/${catlog}/catlog.json`), jsbeautify(JSON.stringify(pkg)), {
-			'indent_with_tabs': true,
-			'indent_size': 4,
-		});
+		// fs.writeFileSync(path.join(__dirname, `../src/${book}/${catlog}/catlog.json`), jsbeautify(JSON.stringify(pkg)), {
+		// 	'indent_with_tabs': true,
+		// 	'indent_size': 4,
+		// });
 		return;
 	}
-	let pinyintitle = pinyin(pkg[index].title.replace(/，|·/g, '')).join('-');
-	fs.writeFile(path.join(__dirname, `../src/${book}/${catlog}/${pinyintitle}.md`), `# ${pkg[index].title}\n\n> ${pkg[index].author}\n`,() => {
-		pkg[index].pinyin = pinyintitle;
+	// let pinyintitle = pinyin(pkg[index].title.replace(/，|·/g, '')).join('-');
+	fs.writeFile(path.join(__dirname, `../src/${book}/${catlog}/${pkg[index].title}.md`), `# ${pkg[index].title}\n\n> ${pkg[index].author}\n`,() => {
+		// pkg[index].pinyin = pinyintitle;
 		index ++;
 		write();
 	})
